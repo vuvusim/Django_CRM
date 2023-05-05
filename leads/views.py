a33f763832f10d5fa7edd3abba1f46d6d6a79503
Simas Venskus <simasvenskus@gmail.com>
2023-05-05 22:41:44 +0300

    Template folder w/ settings tweak

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home_page(request):
+    # return HttpResponse("Hello World!!!")
+    return render(request, "leads/home_page.html")
+
+def second_page(request):
+    return render(request, "second_page.html")
